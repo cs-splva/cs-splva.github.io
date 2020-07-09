47158a3b7bb802faf94419d03052d8f10806722b
xxanctuarii <xxanctuarii@icloud.com>
2020-07-09 19:30:54 +0200

    Added functions for classes
    Added addClass()
    Added removeClass()
    Update on setDocumentProperty() -> added change trigger

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,4 @@
+// Get and Set Document properties
 function getDocumentProperty(ID){
     var idSelection = "#" + ID + " input";
     return $(idSelection).val();
@@ -6,4 +7,16 @@ function getDocumentProperty(ID){
 function setDocumentProperty(ID, value){
     var idSelection = "#" + ID + " input";
     $(idSelection).val(value).focus().blur();
+	$(idSelection).change() // Trigger change event
+}
+
+// Add and remove Classes
+function addClass(ID, className) {
+    var ID = "#" + ID;
+    document.querySelector(ID).classList.add(className);
+}
+
+function removeClass(ID, className) {
+    var ID = "#" + ID;
+    document.querySelector(ID).classList.remove(className);
 }
