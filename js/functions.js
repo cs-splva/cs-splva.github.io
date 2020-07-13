f1fe896679cd54ca8fea1ee3580f072677dd69ae
xxanctuarii <xxanctuarii@icloud.com>
2020-07-13 12:05:07 +0200

    Added function onlyShow()
    Added the function onlyShow() which makes it possible to ony reveal an element with a certain ID from op a group of ID's which share the same class (the class is used to

--- js/functions.js
+++ js/functions.js
@@ -20,3 +20,23 @@ function removeClass(ID, className) {
     var ID = "#" + ID;
     document.querySelector(ID).classList.remove(className);
 }
+
+// Show only one ID from op group of ID's that share the same class
+function onlyShow(ID,classGroup) {
+
+	// Get all elements with class from input
+	var elements = $('.'+classGroup);
+
+	// Loop through all elements
+	// If element is equal to input ID, remove class 'hidden'
+	// Else add class 'hidden'
+	var i;
+	for (i = 0; i < elements.length; i++) {
+	  var currentID = elements[i].id;
+	  if(currentID == ID){
+	    removeClass(currentID,'hidden');
+	  } else {
+	    addClass(currentID,'hidden');
+	  }
+	} 
+}
